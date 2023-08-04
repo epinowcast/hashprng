148914e853465b1d2ede12aed0c79162183a57bf
Sam <s.e.abbott12@gmail.com>
2023-08-04 11:10:35 +0100

    Hotfix: Package logo

--- inst/make_hexsticker.R
+++ inst/make_hexsticker.R
@@ -1,4 +1,5 @@
-library(epinowcast)
+library(hashprng)
+library(data.table)
 library(hexSticker)
 library(sysfonts)
 library(ggplot2)
@@ -7,32 +8,46 @@ library(ggplot2)
 font_add_google("Zilla Slab Highlight", "useme")
 
 # strip out most of the background
-hex_plot <- mtcars |>
+load("inst/basic_vignette.rda")
+
+spag_plot <- reduced_dt[
+  sample <= 250 & 
+  !model %in% "ID" &
+  time <= 40 &
+  !type %in% "SMO"
+][,
+  type := as.factor(type)
+] |> 
+  setorder(type) |>
   ggplot() +
-  aes(x = wt, y = mpg) +
-  geom_hex() +
+  aes(time, averted, color = type, group = interaction(type, sample)) +
+  geom_line(alpha = 0.4) +
+  scale_colour_manual(
+    values = c("HBM" = "#2c3d70", "NON" = "#91acfc")
+  ) +
   scale_x_continuous(breaks = NULL) +
   scale_y_continuous(breaks = NULL) +
   labs(x = "", y = "") +
   theme_void() +
   theme_transparent() +
-  theme(legend.position = "none",
-        panel.background = element_blank())
+  theme(
+    legend.position = "none", panel.background = element_blank()
+  )
 
 # make and save hexsticker
 sticker(
-  hex_plot,
+  spag_plot,
   package = "hashprng",
   p_size = 23,
-  p_color = "#646770",
+  p_color = "#2c3d70",
   s_x = 1,
   s_y = 0.85,
-  s_width = 1.3,
-  s_height = 0.85,
+  s_width = 1.8,
+  s_height = 1.2,
   h_fill = "#ffffff",
-  h_color = "#646770",
+  h_color = "#2c3d70",
   filename = file.path("man", "figures", "logo.png"),
-  url = "",
-  u_color = "#646770",
+  url = "hashprng.epinowcast.org",
+  u_color = "#2c3d70",
   u_size = 3.5
 )
