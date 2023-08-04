6c8bfa9edca999a142743c52e014e7a3c61bb6bc
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-08-04 21:47:16 +0100

    update hex sticker to use vignette data

--- inst/make_hexsticker.R
+++ inst/make_hexsticker.R
@@ -1,19 +1,42 @@
-library(epinowcast)
+
+library(data.table)
 library(hexSticker)
 library(sysfonts)
 library(ggplot2)
 
+.args <- if (interactive()) {
+  c(
+    file.path("inst", "basic_vignette.rda"),
+    file.path("man", "figures", "logo.png")
+  )
+} else {
+  commandArgs(trailingOnly = TRUE)
+}
+
 # font setup
 font_add_google("Zilla Slab Highlight", "useme")
 
+# load some example interesting stochastic model data
+load(.args[1])
+
+slice_dt <- samples_dt[,
+  tm := seq_len(.N) - 1L, by = .(sample, type)
+][ type == "HBM" ]
+
+max_split <- slice_dt[, sample[which.max(not_i - withi)]]
+
+plot_dt <- slice_dt |> melt.data.table(
+  id.vars = c("sample", "type", "tm"),
+  measure.vars = c("not_i", "withi")
+)
+
 # strip out most of the background
-hex_plot <- mtcars |>
+hex_plot <- plot_dt[
+  between(tm, 10, 40) & sample == max_split
+] |>
   ggplot() +
-  aes(x = wt, y = mpg) +
-  geom_hex() +
-  scale_x_continuous(breaks = NULL) +
-  scale_y_continuous(breaks = NULL) +
-  labs(x = "", y = "") +
+  aes(x = tm, y = log(value), fill = variable) +
+  geom_hex(alpha = 0.5) +
   theme_void() +
   theme_transparent() +
   theme(legend.position = "none",
@@ -25,13 +48,13 @@ sticker(
   package = "hbmPRNG",
   p_size = 23,
   p_color = "#646770",
-  s_x = 1,
-  s_y = 0.85,
-  s_width = 1.3,
-  s_height = 0.85,
+  s_x = 0.95,
+  s_y = 1,
+  s_width = 1.35,
+  s_height = 1.0,
   h_fill = "#ffffff",
   h_color = "#646770",
-  filename = file.path("man", "figures", "logo.png"),
+  filename = tail(.args, 1),
   url = "",
   u_color = "#646770",
   u_size = 3.5
